b3ec2ca0a91ce47527716d8c64e83628a35f0cfb
Tom Ritchford <tom@swirly.com>
2025-06-04 18:15:44 +0000

    Final changes from first server

--- torch-all.sh
+++ torch-all.sh
@@ -2,7 +2,13 @@
 
 set -ex
 
-ROOT=$( pwd )
+if [[ $1 ]]; then
+    ROOT=$1
+else
+    ROOT=$(pwd)
+fi
+
+ROOT=${1:=$( pwd )}
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/torch-clone.sh
@@ -15,4 +21,5 @@ fi
 
 export PYTORCH_BUILD_DIRECTORY=$( pwd )
 source $SCRIPT_DIR/torch-env.sh
+conda activate pytorch-dev
 source $SCRIPT_DIR/torch-build.sh
